6fcd40df75fbf079b1454e55836446ee584dd71c
Ian Shelton <ian@Ians-MacBook-Air.local>
2025-04-11 10:44:25 -0400

    update python-app.yml

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CI-CD-Testing
+
+`flake8 app --count --select=E9,F63,F7,F82 --show-source --statistics`
--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
 def multiply_numbers(x, y):
     """Method to multiply the two arguments passed."""
     return x * y
+
